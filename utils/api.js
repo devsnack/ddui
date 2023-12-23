c9baf762514ee97cf1a054c3d257d1c5e86717f5
devsnack <devsnack27@gmail.com>
2023-12-23 13:24:14 +0100

    update srv

--- utils/api.js
+++ utils/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:5000";
+// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://dd-g1u8exm04-jakaissa.vercel.app/";
 
 export const LOGIN_URL = "/api/v1/users/login";
 export const REFRESH_TOKEN = "/api/v1/users/refresh";
-export const newStore = "http://localhost:5000/api/v1/stores";
+export const newStore = "/api/v1/stores";
 export const vsStore = "/api/v1/orders/visite";
 export const newOrder = "/api/v1/orders";
 export default axios.create({
